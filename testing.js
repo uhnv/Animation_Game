3ce6900c7c75967f54f407ee7e05075b7b61786b
Arashpreet123 <51033451+Arashpreet123@users.noreply.github.com>
2023-01-29 18:28:55 -0800

    update

---
+++ testing.js
@@ -0,0 +1,13 @@
+function draw(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "gray";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    var spritesheet = "spritesheet/megaManfull.png";
+    ctx.drawImage(spritesheet,0,0);
+    
+    }
+     function sprite(){
+    
+    
+     }
